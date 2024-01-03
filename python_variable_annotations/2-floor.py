8493b46ebe4ed9dca29b4199f5b3230270e54822
powerofcode2023 <holbertonc21@gmail.com>
2024-01-03 11:15:17 +0100

    Write a type-annotated function floor which takes a float n

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""function floor which takes a float n"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """returns the floor of the float"""
+    return math.floor(n)
