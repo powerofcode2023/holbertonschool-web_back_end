2c0e765666bee46d2bf44be683cc6fe86bff9ed1
powerofcode2023 <holbertonc21@gmail.com>
2024-01-17 11:49:58 +0100

    fix issue

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -4,11 +4,11 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
+    getIncomeInDollars(incomeAmount) {
+      return `$${incomeAmount}`;
     },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
+    getIncomeInEuros(incomeAmount) {
+      return `${incomeAmount} euros`;
     },
   };
 
