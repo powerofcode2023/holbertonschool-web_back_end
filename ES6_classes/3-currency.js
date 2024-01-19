e5d329977768faf8cbd7849c7edf8aff9de531be
powerofcode2023 <holbertonc21@gmail.com>
2024-01-19 11:08:36 +0100

    task3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,27 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set setCode(newCode) {
+    this._code = newCode;
+  }
+
+  set setName(newName) {
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
