19a9a8058e9ec6cd98bc5ada773eefe2a7952a4c
powerofcode2023 <holbertonc21@gmail.com>
2024-01-19 12:22:14 +0100

    task5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+    if (typeof sqft !== 'number') throw TypeError('Sqft must be a number');
+    this._sqft = sqft;
+  }
+
+  // Getter
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Setter
+
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') throw TypeError('Sqft must be a number');
+    this._sqft = newSqft;
+  }
+}
